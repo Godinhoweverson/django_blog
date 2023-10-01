c4034a5c45d4911073cb50bef3e0de8b6246221b
Godinhoweverson <>
2023-10-01 11:30:26 +0000

    d

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['godinho-django-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['godinho-django-blog.herokuapp.com', 'django_blog']
 
 
 # Application definition
